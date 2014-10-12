c61803313f66bdfc1abb3e8830c64cd03d794656
yangxian10 <yangxian10@gmail.com>
2014-10-12 14:08:31 +0800

    add db.py function

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,16 +1,130 @@
+__author__ = 'yangxian'
+
+import logging, threading, functools
+
 class _Engine(object):
 	def __init__(self, connect):
 		self._connect = connect
+
 	def connect(self):
 		return self._connect()
 
-engine = None
-
 class _DbCtx(threading.local):
 	"""docstring for _DbCtx"""
+
 	def __init__(self):
 		#super _DbCtx, self).__init__()
 		self.connection = None
 		self.transactions = 0
+
 	def is_init(self):
 		return not self.connection is None
+
+	def init(self):
+        logging.info('open lazy connection...')
+		self.connection = _LasyConnection()
+		self.transactions = 0
+
+	def cleanup(self):
+		self.connection.cleanup()
+		self.connection = None
+
+	def cursor(self):
+		return self.connection.cursor()
+
+class _ConnectionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_cleanup = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_cleanup = True
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        global _db_ctx
+        if self.should_cleanup:
+            _db_ctx.cleanup()
+
+class _TransactionCtx(object):
+    def __enter__(self):
+        global _db_ctx
+        self.should_close_conn = False
+        if not _db_ctx.is_init():
+            _db_ctx.init()
+            self.should_close_conn = True
+        _db_ctx.transactions = _db_ctx.transactions + 1
+        logging.info('begin transaction...' if _db_ctx.transactions == 1 else 'join current transaction...')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        global _db_ctx
+        _db_ctx.transactions = _db_ctx.transactions - 1
+        try:
+            if _db_ctx.transactions == 0:
+                if exc_type is None:
+                    self.commit()
+                else:
+                    self.rollback()
+        finally:
+            if self.should_close_conn:
+                _db_ctx.cleanup()
+
+    def commit(self):
+        global _db_ctx
+        logging.info('commit transaction...')
+        try:
+            _db_ctx.connection.commit()
+            logging.info('commit ok.')
+        except:
+            logging.warning('commit failed. try rollback...')
+            _db_ctx.connection.rollback()
+            logging.warning('rollback ok.')
+            raise
+
+    def rollback(self):
+        global _db_ctx
+        logging.warning('rollback transaction...')
+        _db_ctx.connection.rollback()
+        logging.info('rollback ok.')
+
+engine = None
+_db_ctx = _DbCtx()
+
+def connection():
+    return _ConnectionCtx()
+
+def with_connection(func):
+    @functools.wraps(func)
+    def _warpper(*args, **kw):
+        with _ConnectionCtx():
+            return func(*args, **kw)
+    return _warpper
+
+def transaction():
+    return _TransactionCtx()
+
+def with_transaction(func):
+    @functools.wraps(func)
+    def _warpper(*args,)
+
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+    import mysql.connector
+    global engine
+    if engine is not None:
+        raise DBError('Engine is already initialized.')
+    params = dict(user=user, password=password, database=database, host=host, port=port)
+    defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+    for k,v in defaults.iteritems():
+        params[k] = kw.pop(k,v)
+    params.update(kw)
+    params['buffered'] = True
+    engine = _Engine(lambda : mysql.connector.connect(**params))
+    logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
+def update(sql, *args):
+    return _update(sql, *args)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    create_engine('www-data', 'www-data', 'test')
