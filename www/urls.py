82db5242906d1db0669091bbe83715a78ffc3658
yangxian10 <yangxian10@gmail.com>
2014-10-16 21:51:28 +0800

    add api

--- www/urls.py
+++ www/urls.py
@@ -1,9 +1,9 @@
 __author__ = 'yang xian'
 
-import logging
+import logging, os, re, time, base64, hashlib
 
-from transwarp.web import get, view
-
+from transwarp.web import get, view, post, ctx, interceptor, seeother, notfound
+from apis import api, APIError, APIValueError, APIPermissionError, APIResourceNotFoundError
 from models import User, Blog, Comment
 
 @view('blogs.html')
@@ -12,3 +12,11 @@ def index():
     blogs = Blog.find_all()
     user = User.find_first('where email=?', 'admin@example.com')
     return dict(blogs=blogs, user=user)
+
+@api
+@get('/api/users')
+def api_get_users():
+    users = User.find_by('order by created_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
